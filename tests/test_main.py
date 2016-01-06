aec2404926ccaa231b3d8a64482e82e8c79078a0
Andrew Kuchev <0coming.soon@gmail.com>
2016-01-06 15:08:20 +0500

    Added failing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,7 @@ import bpy
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(1, len(bpy.data.scenes))
+        self.fail('Whoops')
 
 
 if __name__ == '__main__':
