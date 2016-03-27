86f40c8b6246c2c2e7924d505b1f7a1d39dcc6be
Andrew Kuchev <0coming.soon@gmail.com>
2016-03-27 14:36:36 +0500

    Changed default color FIXME: changed nozzle height Always create new nozzles at the scene center

--- src/operators/add_nozzle_operator.py
+++ src/operators/add_nozzle_operator.py
@@ -2,7 +2,7 @@ import bpy
 
 from ..core import LifContext
 from ..models import FountainModel
-from ..utils.bpy_helpers import create_bpy_cylinder, get_red_color, create_bpy_color
+from ..utils.bpy_helpers import create_bpy_cylinder, create_bpy_color
 from ..utils.optional import Optional
 
 
@@ -14,15 +14,23 @@ class AddNozzleOperator(bpy.types.Operator):
     bl_label = "Add nozzle"
 
     def create_nozzle(self, context, name=None, parent=None):
-        nozzle = create_bpy_cylinder(context, name=name, parent=parent, scale=[0.1, 0.1, 0.2])
+        nozzle = create_bpy_cylinder(context, name=name, parent=parent, scale=[0.1, 0.1, 3])
+        bpy.context.scene.cursor_location = bpy.context.active_object.location
+        bpy.context.active_object.location.z -= 2.9
+        bpy.context.scene.cursor_location.z += 1
         water = create_bpy_cylinder(context, name=LifContext.nozzle_water_part_name,
-                                    parent=nozzle, scale=[0.9, 0.9, 1.1])
+                                    parent=nozzle, scale=[0.8, 0.8, 0.5])
         bpy.ops.object.constraint_add(type='COPY_LOCATION')
         water.constraints['Copy Location'].target = nozzle
         water.constraints['Copy Location'].use_x = True
         water.constraints['Copy Location'].use_y = True
         water.constraints['Copy Location'].use_z = False
-        water.data.materials.append(create_bpy_color(nozzle.name, (1, 0, 0)))
+        water.data.materials.append(create_bpy_color(nozzle.name, (0.1, 0.5, 1)))
+        water.select = False
+        nozzle.select = True
+        bpy.context.scene.objects.active = nozzle
+        bpy.context.scene.cursor_location = nozzle.location
+        bpy.context.scene.cursor_location = (0, 0, 0)
         return nozzle
 
     def execute(self, context):
